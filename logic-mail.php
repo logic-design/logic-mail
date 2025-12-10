d71a8d5ead81ec641f572e480c8373392aecb0a7
albiegibbs <77971896+albiegibbs@users.noreply.github.com>
2025-12-10 13:12:18 +0000

    fix gravity forms

--- logic-mail.php
+++ logic-mail.php
@@ -9,66 +9,102 @@ Author URI: https://www.logicdesign.co.uk/
 
 class Logic_Mail
 {
-	private $name = 'Logic_ Mail Notifications';
-	private $email = 'noreply@logic-mail.co.uk';
+    private $name  = 'Logic_ Mail Notifications';
+    private $email = 'noreply@logic-mail.co.uk';
 
-	public function __construct()
-	{
-		add_filter('wp_mail_from', [$this, 'filter_mail_from']);
-		add_filter('wp_mail_from_name', [$this, 'filter_mail_from_name']);
-		add_action('phpmailer_init', [$this, 'action_phpmailer_init']);
-		add_filter('wpcf7_mail_components', [$this, 'filter_wpcf7_mail_components'], 10, 2);
-		add_filter('wpforms_email_sender_address', [$this, 'filter_wpforms_email_sender_address']);
-		add_filter('wpforms_email_sender_name', [$this, 'filter_wpforms_email_sender_name']);
-		add_filter('wpforms_email_headers', [$this, 'filter_wpforms_email_headers']);
-		add_filter('gform_pre_send_email', [$this, 'filter_gform_pre_send_email']);
-	}
+    public function __construct()
+    {
+        add_filter('wp_mail_from', [$this, 'filter_mail_from']);
+        add_filter('wp_mail_from_name', [$this, 'filter_mail_from_name']);
+        add_action('phpmailer_init', [$this, 'action_phpmailer_init']);
+        add_filter('wpcf7_mail_components', [$this, 'filter_wpcf7_mail_components'], 10, 2);
 
-	public function filter_mail_from($original_email_address)
-	{
-		return $this->email;
-	}
+        // WPForms
+        add_filter('wpforms_email_sender_address', [$this, 'filter_wpforms_email_sender_address']);
+        add_filter('wpforms_email_sender_name', [$this, 'filter_wpforms_email_sender_name']);
+        add_filter('wpforms_email_headers', [$this, 'filter_wpforms_email_headers']);
 
-	public function filter_mail_from_name($original_email_from)
-	{
-		return $this->name;
-	}
+        // Gravity Forms
+        add_filter('gform_pre_send_email', [$this, 'filter_gform_pre_send_email']);
+    }
 
-	public function action_phpmailer_init($phpmailer)
-	{
-		$phpmailer->setFrom($this->email, $this->name, false);
-	}
+    public function filter_mail_from($original_email_address)
+    {
+        return $this->email;
+    }
 
-	public function filter_wpcf7_mail_components($components, $contactForm)
-	{
-		$components['additional_headers'] = "From: {$this->name} <{$this->email}>\r\n";
-		return $components;
-	}
+    public function filter_mail_from_name($original_email_from)
+    {
+        return $this->name;
+    }
 
-	public function filter_wpforms_email_sender_address($sender)
-	{
-		return $this->email;
-	}
+    public function action_phpmailer_init($phpmailer)
+    {
+        $phpmailer->setFrom($this->email, $this->name, false);
+    }
 
-	public function filter_wpforms_email_sender_name($name)
-	{
-		return $this->name;
-	}
+    public function filter_wpcf7_mail_components($components, $contactForm)
+    {
+        $components['additional_headers'] = "From: {$this->name} <{$this->email}>\r\n";
+        return $components;
+    }
 
-	public function filter_wpforms_email_headers($headers)
-	{
-		$headers = preg_replace('/^From:.*$/m', '', $headers);
-		$headers .= "From: {$this->name} <{$this->email}>\r\n";
-		return $headers;
-	}
+    // -----------------------------
+    // WPForms
+    // -----------------------------
 
-	public function filter_gform_pre_send_email($email)
-	{
-		$email['from']     = $this->email;
-		$email['fromName'] = $this->name;
-		$email['headers']  = "From: {$email['fromName']} <{$email['from']}>\r\n";
-		return $email;
-	}
+    public function filter_wpforms_email_sender_address($sender)
+    {
+        return $this->email;
+    }
+
+    public function filter_wpforms_email_sender_name($name)
+    {
+        return $this->name;
+    }
+
+    public function filter_wpforms_email_headers($headers)
+    {
+        // Remove existing From header
+        $headers = preg_replace('/^From:.*$/m', '', $headers);
+        $headers .= "From: {$this->name} <{$this->email}>\r\n";
+        return $headers;
+    }
+
+    // -----------------------------
+    // Gravity Forms (FIXED)
+    // -----------------------------
+
+    public function filter_gform_pre_send_email($email)
+    {
+        $email['from']     = $this->email;
+        $email['fromName'] = $this->name;
+
+        $fromHeader = "From: {$email['fromName']} <{$email['from']}>\r\n";
+
+        // Handle headers as string or array
+        if (isset($email['headers']) && is_array($email['headers'])) {
+
+            // Remove existing From:
+            $email['headers'] = array_filter($email['headers'], function ($h) {
+                return stripos($h, 'From:') !== 0;
+            });
+
+            $email['headers'][] = $fromHeader;
+
+        } else {
+
+            // Treat as string
+            $existing = $email['headers'] ?? '';
+
+            // Remove any previous From:
+            $existing = preg_replace('/^From:.*$/m', '', $existing);
+
+            $email['headers'] = $existing . $fromHeader;
+        }
+
+        return $email;
+    }
 }
 
 new Logic_Mail();
